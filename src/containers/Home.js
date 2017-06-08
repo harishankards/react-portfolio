b600160739c6729908c92ea0a3f63c2d34f818b9
harishankards <harishankards@gmail.com>
2017-06-08 17:03:47 +0530

    Introduce styled-components pkg and add little css. Also start modularizing

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,28 @@
 import React, {Component, PropTypes} from 'react';
+import styled from 'styled-components';
+import Container from '../theme/grid/Container';
+
+const Image = styled.img`
+  width: 100%;
+`;
 
 export default class Home extends Component {
   static propTypes = {};
 
   render(){
     return(
-      <div>
-        Hi! This is your home
-      </div>
+      <Container>
+        <h1> Hi! Home </h1>
+        <Image src={require('../assets/hero.jpg')} alt = "octodex-image" />
+        <p> Hi Guys! This is HS. Just a random blah blah blah blah blah blah
+            blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+            blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+            blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+            blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+            blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+            guy in the world and also in the internet
+        </p>
+      </Container>
     );
   }
 
--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
+import './theme/globalStyle';
+
 ReactDOM.render(routes, document.getElementById('root'));
 registerServiceWorker();

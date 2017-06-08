bca8dd4810982fb1ee59079c6510da8e9583225b
harishankards <harishankards@gmail.com>
2017-06-08 16:10:48 +0530

    Add Home component with basic route functionality and remove useless default codes

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class Home extends Component {
+  static propTypes = {};
+
+  render(){
+    return(
+      <div>
+        Hi! This is your home
+      </div>
+    );
+  }
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById('root'));
 registerServiceWorker();

675d9d04339d709aae157e143ae0a3a81e9c94ef
harishankards <harishankards@gmail.com>
2017-06-10 19:29:37 +0530

    add media queries to make project page responsive

--- src/theme/grid.js
+++ src/theme/grid.js
@@ -1,4 +1,5 @@
 import styled, {css} from 'styled-components';
+import media from './media';
 
 export const Div = styled.div`
   ${({marginBottom}) => marginBottom && css`
@@ -13,6 +14,17 @@ export const Container = styled(Div)`
   padding-right: 120px;
   padding-top: 30px;
 
+  ${media.tablet`
+    padding-left: 30px;
+    padding-right: 30px;
+    padding-top:15px;
+  `}
+  ${media.phone`
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top:15px;
+  `}
+  
 `;
 
 export const Relative = styled(Div)`

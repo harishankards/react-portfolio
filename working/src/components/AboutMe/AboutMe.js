0266a816daa942cc373ad05efb6fd8f058a2cf65
harishankards <harishankards@gmail.com>
2017-06-14 20:42:06 +0530

    Add styles for h1,p for about me

--- working/src/components/AboutMe/AboutMe.js
+++ working/src/components/AboutMe/AboutMe.js
@@ -1,13 +1,18 @@
 import React,{Component} from 'react';
-import {Container} from '../../theme/grid';
-import {Title} from './AboutMe.style'
+import {Container,Flex} from '../../theme/grid';
+import {Title} from './AboutMe.style';
+import {H2,P} from '../../theme/types';
+
 
 class AboutMe extends Component{
   render(){
     return(
       <Container>
         <Title>  About me!</Title>
-        <h2>Hey dudes! Do you watch movies? Duck yeah I do!</h2>
+        <H2 align="left">Hey dudes! Do you watch movies? Duck yeah I do!</H2>
+        <Flex>
+          <P align="right">Do you wanna build a snowman?</P>
+        </Flex>
       </Container>
     );
   }
--- working/src/components/AboutMe/AboutMe.style.js
+++ working/src/components/AboutMe/AboutMe.style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import {red} from '../../theme/variables';
+import {Relative} from '../../theme/grid';
 
 export const Title=styled.h1`
   margin-top:0;
